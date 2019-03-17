52eb958d1923010762091fdcabc8746ebf68e60c
hashirsarwar <45683359+hashirsarwar@users.noreply.github.com>
2019-03-17 01:58:31 -0700

    Update View.java

--- src/com/tictactoe/View.java
+++ src/com/tictactoe/View.java
@@ -3,7 +3,6 @@ package com.tictactoe;
 import javax.swing.JFrame;
 import java.awt.GridLayout;
 import javax.swing.JButton;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
